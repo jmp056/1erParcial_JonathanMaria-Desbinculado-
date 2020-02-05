37de6059349598600282d54b9879daad34bfd815
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2020-02-05 18:38:12 -0400

    Entidad articulos agregada al contexto

--- 1erParcial_JonathanMaria/DAL/Contexto.cs
+++ 1erParcial_JonathanMaria/DAL/Contexto.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using _1erParcial_JonathanMaria.Data;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,6 +9,7 @@ namespace _1erParcial_JonathanMaria.DAL
 {
     public class Contexto : DbContext
     {
+        public DbSet<Articulos> Articulos { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(@"Server = JMP-PC\SQLEXPRESS; Database = 1erParcial_JonathanMaria; Trusted_Connection = true");
