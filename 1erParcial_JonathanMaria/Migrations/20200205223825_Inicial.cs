5cd5439a54324340a89222f11b073472c7102684
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2020-02-05 18:38:41 -0400

    Migracion de la base de datos creada

---
+++ 1erParcial_JonathanMaria/Migrations/20200205223825_Inicial.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace _1erParcial_JonathanMaria.Migrations
+{
+    public partial class Inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Articulos",
+                columns: table => new
+                {
+                    ArticuloId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Fecha = table.Column<DateTime>(nullable: false),
+                    Descripcion = table.Column<string>(nullable: true),
+                    Costo = table.Column<float>(nullable: false),
+                    Ganancia = table.Column<float>(nullable: false),
+                    Precio = table.Column<float>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Articulos", x => x.ArticuloId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Articulos");
+        }
+    }
+}
