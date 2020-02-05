1b017766217433cbc5e54ec456ae1054694aeec2
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2020-02-05 17:40:43 -0400

    Contexto listo

---
+++ 1erParcial_JonathanMaria/DAL/Contexto.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _1erParcial_JonathanMaria.DAL
+{
+    public class Contexto : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server = JMP-PC\SQLEXPRESS; Database = 1erParcial_JonathanMaria; Trusted_Connection = true"); 
+        }
+    }
+}
